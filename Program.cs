2385fc81a6bbfeed064e60608bb5b11bc91b1f32
JordanSuSu <104271700+JordanSuSu@users.noreply.github.com>
2022-08-16 15:13:50 +1000

    add some simple class structure
    1. add Board class
    2. add Player class

--- Program.cs
+++ Program.cs
@@ -1,5 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(" superjohn")
-Console.WriteLine(" Ellen")
-Console.WriteLine("Jordan")
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine(" superjohn")
+// Console.WriteLine(" Ellen")
+// Console.WriteLine("Jordan")
+
+using System;
+
+namespace ChessGame // Note: actual namespace depends on the project name.
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Board {
+    }
+
+    class History : Board{
+        //Save all moves
+        protected Stack<string> forwardTrack = new Stack<string>();
+        //Save the moves which are undo by user
+        protected Queue<string> backTrack = new Queue<string>();
+    }
+
+    class Chess : Board {
+
+    }
+
+
+    class Player{
+
+    }
+
+    class Human : Player{
+
+    }
+
+    class Computer : Player{
+        
+    }
+}
